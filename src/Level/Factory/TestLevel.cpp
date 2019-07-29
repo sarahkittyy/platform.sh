83436b4552c154ce023a41b684068153b3a3ccbb
sarahkittyy <sarahohlin1@gmail.com>
2019-07-29 18:33:15 -0400

    moved player object for testing

--- src/Level/Factory/TestLevel.cpp
+++ src/Level/Factory/TestLevel.cpp
@@ -7,6 +7,7 @@ Level testLevel(sf::RenderWindow* window, ResourceManager* resource)
 {
 	Level lvl;
 	lvl.init(window, resource, "assets/maps/test_map.json", true);
+	lvl.addObject(new Object::Player({ 4, 3 }));
 	return lvl;
 }
 

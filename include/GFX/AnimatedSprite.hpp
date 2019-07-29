ab04479050d5499002e8c602181b364457883537
sarahkittyy <sarahohlin1@gmail.com>
2019-07-29 00:29:34 -0400

    added animatedsprites

---
+++ include/GFX/AnimatedSprite.hpp
@@ -0,0 +1,111 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+namespace GFX
+{
+
+/**
+ * @brief Animateable sprite based around FSMs, with customizable animation states.
+ * 
+ */
+class AnimatedSprite : public sf::Drawable, public sf::Transformable
+{
+public:
+	/// An animation structure.
+	struct Animation
+	{
+		std::vector<int> frames;   // Animation frames
+		sf::Time speed;			   // Time per frame. (-1 for unmoving)
+	};
+
+	/// Init
+	AnimatedSprite();
+
+	/**
+	 * @brief Initialize the sprite.
+	 * 
+	 * @param texture The sprite texture map.
+	 * @param size The size of a single animation frame.
+	 */
+	void init(sf::Texture* texture, sf::Vector2f size);
+
+	/**
+	 * @brief Update the animation. Call once per frame.
+	 * 
+	 */
+	void update();
+
+	/**
+	 * @brief Add an animation to the sprite.
+	 * 
+	 * @param name The name of the animation.
+	 * @param animation The animation data.
+	 */
+	void addAnimation(std::string name, Animation animation);
+
+	/**
+	 * @brief Remove an animation.
+	 * 
+	 * @param name The name of the animation.
+	 */
+	void removeAnimation(std::string name);
+
+	/**
+	 * @brief Set the currently running animation.
+	 * 
+	 * @param name The name of the animation.
+	 */
+	void setAnimation(std::string name);
+
+	/**
+	 * @brief Start the animation.
+	 * 
+	 */
+	void start();
+
+	/**
+	 * @brief Stop the animation.
+	 * 
+	 */
+	void stop();
+
+private:
+	/// SFML draw() override.
+	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+	/// The sprite itself.
+	sf::Sprite mSprite;
+	/// The sprite's texture.
+	sf::Texture* mTexture;
+	/// The size of a single frame.
+	sf::Vector2f mFrameSize;
+	/// The size of the texture map, in frames.
+	sf::Vector2i mGridSize;
+
+	/// Get the current animation's animation rect for the current frame.
+	sf::IntRect getAnimationRect();
+	/// Get the current animation, or DEFAULT_ANIMATION if it's not set.
+	Animation* getCurrentAnimation();
+
+	/// All stored animations.
+	std::unordered_map<std::string, Animation> mAnimations;
+	/// The current animation.
+	Animation* mCurrentAnimation;
+	/// The name of the currently running animation.
+	std::string mCurrentAnimationName;
+	/// The frame of the current animation.
+	int mCurrentFrame;
+	/// Animation clock.
+	sf::Clock mClock;
+	/// Whether or not the animation is running.
+	bool mRunning = false;
+
+
+	/// The default animation (first frame, -1 speed).
+	Animation DEFAULT_ANIMATION;
+};
+
+}
---
+++ src/GFX/AnimatedSprite.cpp
@@ -0,0 +1,126 @@
+#include "GFX/AnimatedSprite.hpp"
+
+namespace GFX
+{
+
+AnimatedSprite::AnimatedSprite()
+	: mCurrentAnimation(nullptr),
+	  mCurrentAnimationName(""),
+	  mCurrentFrame(0),
+	  DEFAULT_ANIMATION({.frames = {0}, .speed = sf::seconds(-1)})
+{
+	mClock.restart();
+}
+
+void AnimatedSprite::init(sf::Texture* texture, sf::Vector2f size)
+{
+	mTexture = texture;
+	mFrameSize = size;
+
+	// Init the sprite
+	mSprite.setTexture(*mTexture);
+	mSprite.setTextureRect(sf::IntRect(0, 0, mFrameSize.x, mFrameSize.y));
+
+	// Calculate the texture grid size.
+	mGridSize = {
+		(int)mTexture->getSize().x / (int)mFrameSize.x,
+		(int)mTexture->getSize().y / (int)mFrameSize.y};
+}
+
+void AnimatedSprite::update()
+{
+	if (!mRunning || getCurrentAnimation()->speed.asSeconds() == -1)
+		return;
+
+	// If the clock is ready to update...
+	if (mClock.getElapsedTime() > getCurrentAnimation()->speed)
+	{
+		// Increment the current frame
+		mCurrentFrame++;
+		// If it's at or above the size of the frames of this animation,
+		// loop it back to 0.
+		if (mCurrentFrame >= getCurrentAnimation()->frames.size())
+		{
+			mCurrentFrame = 0;
+		}
+
+		mClock.restart();
+
+		// Set the sprite's rect.
+		mSprite.setTextureRect(getAnimationRect());
+	}
+}
+
+void AnimatedSprite::addAnimation(std::string name, AnimatedSprite::Animation animation)
+{
+	mAnimations[name] = animation;
+}
+
+void AnimatedSprite::removeAnimation(std::string name)
+{
+	mAnimations.erase(name);
+}
+
+void AnimatedSprite::setAnimation(std::string name)
+{
+	// Return if the animation is already running.
+	if (name == mCurrentAnimationName)
+	{
+		return;
+	}
+	// Set the animation name
+	mCurrentAnimationName = name;
+	// Get the animation
+	mCurrentAnimation = &mAnimations[name];
+	// Reset the current frame & the clock.
+	mClock.restart();
+	mCurrentFrame = 0;
+}
+
+void AnimatedSprite::start()
+{
+	mCurrentFrame = 0;
+	mClock.restart();
+	mRunning = true;
+}
+
+void AnimatedSprite::stop()
+{
+	mRunning = false;
+}
+
+sf::IntRect AnimatedSprite::getAnimationRect()
+{
+	int id = getCurrentAnimation()->frames[mCurrentFrame];
+	// X/Y pos of the frame in the texture, in units of frames.
+	int x, y;
+	x = id % mGridSize.x;
+	y = id / mGridSize.y;
+	// Now get them in pixel size.
+	x *= mFrameSize.x;
+	y *= mFrameSize.y;
+	// Return the IntRect
+	return sf::IntRect(x, y, mFrameSize.x, mFrameSize.y);
+}
+
+AnimatedSprite::Animation* AnimatedSprite::getCurrentAnimation()
+{
+	if (mAnimations.size() == 0 ||
+		mCurrentAnimation == nullptr ||
+		mCurrentAnimationName == "")
+	{
+		return &DEFAULT_ANIMATION;
+	}
+	else
+	{
+		return mCurrentAnimation;
+	}
+}
+
+void AnimatedSprite::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+	states.transform *= getTransform();
+	target.draw(mSprite, states);
+}
+
+}

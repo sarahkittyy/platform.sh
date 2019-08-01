e055cdb3013c1333ec51c9ddccc0db17f110e196
sarahkittyy <sarahohlin1@gmail.com>
2019-07-31 21:48:08 -0400

    made some methods const

--- src/Object/Props.cpp
+++ src/Object/Props.cpp
@@ -14,12 +14,12 @@ void Props::set(const nlohmann::json::value_type& data)
 }
 
 bool Props::test(const nlohmann::json::json_pointer& key,
-				 const nlohmann::json::value_type& value)
+				 const nlohmann::json::value_type& value) const
 {
 	return mProps[key] == value;
 }
 
-const nlohmann::json& Props::get()
+const nlohmann::json& Props::get() const
 {
 	return mProps;
 }

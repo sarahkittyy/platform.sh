e1bb8018a6e1f87d4d64d70e8f8851ad66fc58b5
sarahkittyy <sarahohlin1@gmail.com>
2019-08-02 01:13:54 -0400

    testing changes

--- src/Level/Factory/TestLevel.cpp
+++ src/Level/Factory/TestLevel.cpp
@@ -8,9 +8,9 @@ void testLevel(Level* lvl,
 			   ResourceManager* resource)
 {
 	lvl->init(window, resource);
-	lvl->addObject(new Object::Player({ 5.f, 4.f }));
+	lvl->addObject(new Object::Player({ 10.f, 2.f }));
 	lvl->addObject(new Object::Tilemap("assets/maps/test_map.json", true));
-	lvl->addObject(new Object::ArrowPlatform({ 40.f, 4.f }, Object::ArrowPlatform::LEFT));
+	lvl->addObject(new Object::ArrowPlatform({ 20.f, 4.f }, Object::ArrowPlatform::LEFT));
 }
 
 }

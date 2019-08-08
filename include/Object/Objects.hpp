398e036d973f7a772c0f5b7af8a588d24844bfa7
sarahkittyy <sarahohlin1@gmail.com>
2019-08-08 13:21:02 -0400

    added universal objects header

---
+++ include/Object/Objects.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "Object/ArrowPlatform.hpp"
+#include "Object/ArrowPlatformEnd.hpp"
+#include "Object/Object.hpp"
+#include "Object/Player.hpp"
+#include "Object/Tilemap.hpp"
